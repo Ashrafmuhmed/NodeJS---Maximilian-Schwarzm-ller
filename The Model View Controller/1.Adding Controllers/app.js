f35cf8054b84fd4225d4a246bf6ef73712f3f9f8
ashraf <bakr29420@gmail.com>
2025-01-15 19:34:52 +0200

    i

---
+++ The Model View Controller/1.Adding Controllers/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+const rootDir = require('./utils/path');
+const adminData = require('./routers/admin');
+const shopRoute = require('./routers/shop');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(rootDir, 'views'));
+
+app.use(express.static(path.join(rootDir, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminData.routers);
+app.use('/users', shopRoute);
+app.use('/', (req, res, next) => {
+    res.status(404).render('404', {
+        pageTitle: 'Not found page',
+        path : '404'
+    });
+})
+
+app.listen(3000);
