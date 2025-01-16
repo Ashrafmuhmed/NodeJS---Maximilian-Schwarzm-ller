752d0037f685e03ad5247639a8dd4d65e73ecb76
ashraf <bakr29420@gmail.com>
2025-01-16 14:55:55 +0200

    Refactor product model to improve data fetching and saving logic

--- The Model View Controller/1.Adding Controllers/models/product.js
+++ The Model View Controller/1.Adding Controllers/models/product.js
@@ -1,7 +1,24 @@
-const fs = require('fs'); 
-const path = require('path') ;
+const fs = require('fs');
+const path = require('path');
 const rootDir = require('../utils/path');
-const p = path.join(rootDir , 'data' , 'products.json') ;
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = (cb) => {
+    let products = [];
+    fs.readFile(
+        p, (err, data) => {
+            if (!err) {
+                products = JSON.parse(data);
+                console.log('From fetching data', products);
+                cb(products);
+            }
+            else {
+                console.log(err);
+                cb(products);
+            }
+        }
+    )
+}
 
 module.exports = class Product {
     constructor(title) {
@@ -9,37 +26,16 @@ module.exports = class Product {
     }
 
     save() {
-        fs.readFile(p , 
-            (err , data) => {
-                let products = [] ;
-                if(!err) {
-                    products = JSON.parse(data);
-                    console.log(products) ;
-                }
-                products.push(this) ;
-                fs.writeFile(p , JSON.stringify(products) , (err) => {
-                    console.log(err) ;
-                }) ;
-            }
-        );
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            })
+        })
     }
-
-    static fetchAll(cb)
-    {
-        let products = [] ;
-        fs.readFile(
-            p , (err , data) => {
-                if(!err) { 
-                    products = JSON.parse(data);
-                    console.log('From fetching data' , products);
-                    cb(products) ;
-                }
-                else {
-                    console.log(err) ;
-                    cb(products) ;
-                }
-            }
-        )
+    
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 };
 
