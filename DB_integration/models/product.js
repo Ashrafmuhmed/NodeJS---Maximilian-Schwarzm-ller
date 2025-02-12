28d2eb13b7c73b9b90d202d670859288d011a510
ashraf <bakr29420@gmail.com>
2025-02-12 15:05:01 +0200

    Refactor product model: migrate to Sequelize for improved ORM structure and functionality

--- DB_integration/models/product.js
+++ DB_integration/models/product.js
@@ -1,33 +1,33 @@
-const database = require('../utils/database');
+const {Sequelize , DataTypes} = require('sequelize') ; 
 
+const sequelize = require('../utils/database') ;
 
-module.exports = class Product {
-    constructor(id ,title , price , imageUrl , description ) {
-        this.id = id ;
-        this.title = title;
-        this.price = Number.parseInt(price);
-        this.imageUrl = imageUrl ;
-        this.description = description ;
+const Product = sequelize.define(
+    'product' , 
+    {
+        id : {
+            type : DataTypes.INTEGER , 
+            allowNull : false ,
+            autoIncrement : true ,
+            primaryKey : true 
+        },
+        title : {
+            type : DataTypes.STRING , 
+            allowNull : false ,
+        },
+        description : {
+            type : DataTypes.STRING ,
+            allowNull : false 
+        },
+        price : {
+            type : DataTypes.INTEGER , 
+            allowNull : false 
+        },
+        imageUrl : {
+            type :DataTypes.STRING,
+            allowNull : false
+        }
     }
+);
 
-    save() {
-        return database.execute(
-            "INSERT INTO products (title , price , description , imageUrl) VALUE ( ? , ? , ? , ? )",
-            [this.title , this.price , this.description , this.imageUrl]
-        );
-    }
-    
-    static fetchAll() {
-        return database.execute('SELECT * FROM products');
-    }
-
-    static getProductById(id){
-        return database.execute(`SELECT * FROM products WHERE id = ?` , [id]);
-    }
-
-    static deleteProduct( id){
-        
-    }
-};
-
-
+module.exports = Product ; 
